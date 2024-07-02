6f26ad4285c06ae008a3a02941ac580cc3cd67c0
Chester Curme <chester.curme@gmail.com>
2024-07-02 10:08:15 -0400

    remove unused import

--- libs/upstage/tests/integration_tests/test_chat_models_standard.py
+++ libs/upstage/tests/integration_tests/test_chat_models_standard.py
@@ -2,7 +2,6 @@
 
 from typing import Type
 
-import pytest
 from langchain_core.language_models import BaseChatModel
 from langchain_standard_tests.integration_tests import ChatModelIntegrationTests
 
