f3722a499520d7c5313ce54a760c2a92767a8ce2
김주호 <61442066+eora21@users.noreply.github.com>
2025-07-28 16:01:58 +0900

    update document parse base url & remove fragile test (#50)
    * update document parse base url
    
    * fix expected element count in parse test
    
    * update version
    
    * remove fragile tests dependent on model outputs

--- libs/upstage/tests/integration_tests/test_document_parse.py
+++ libs/upstage/tests/integration_tests/test_document_parse.py
@@ -1,15 +1,6 @@
 from pathlib import Path
-from typing import get_args
-
-import pytest
 
 from langchain_upstage.document_parse import UpstageDocumentParseLoader
-from langchain_upstage.document_parse_parsers import (
-    OCR,
-    Category,
-    OutputFormat,
-    SplitType,
-)
 
 EXAMPLE_PDF_PATH = Path(__file__).parent.parent / "examples/solar.pdf"
 
@@ -24,33 +15,3 @@ def test_file_not_found_error() -> None:
         assert False
     except FileNotFoundError:
         assert True
-
-
-@pytest.mark.parametrize("output_format", get_args(OutputFormat))
-@pytest.mark.parametrize("split", get_args(SplitType))
-@pytest.mark.parametrize("ocr", get_args(OCR))
-@pytest.mark.parametrize("coordinates", [True, False])
-@pytest.mark.parametrize("base64_encoding", ["paragraph"])
-def test_document_parse(
-    output_format: OutputFormat,
-    split: SplitType,
-    ocr: OCR,
-    coordinates: bool,
-    base64_encoding: Category,
-) -> None:
-    loader = UpstageDocumentParseLoader(
-        file_path=EXAMPLE_PDF_PATH,
-        output_format=output_format,
-        split=split,
-        ocr=ocr,
-        coordinates=coordinates,
-        base64_encoding=[base64_encoding],
-    )
-    documents = loader.load()
-    if split == "element":
-        if ocr == "auto":
-            assert len(documents) == 14
-        else:
-            assert len(documents) == 15
-    else:
-        assert len(documents) == 1
