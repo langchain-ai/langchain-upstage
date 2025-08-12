3d17d9356da08edd4414878feb9ab2ded2c8f951
김주호 <eora21@naver.com>
2025-08-12 13:20:19 +0900

    change parameter name `input_path` to `input_url`

--- libs/upstage/langchain_upstage/tools/information_extraction_check.py
+++ libs/upstage/langchain_upstage/tools/information_extraction_check.py
@@ -8,21 +8,21 @@ MEGABYTE = 1024 * KILOBYTE
 
 
 def _process_input(
-    input_path: str, supported_extensions: List[str], max_file_size: int
+    input_url: str, supported_extensions: List[str], max_file_size: int
 ) -> str:
-    if re.match(r"^https?://", input_path):
-        return input_path
+    if re.match(r"^https?://", input_url):
+        return input_url
 
-    if os.path.exists(input_path):
-        if os.path.getsize(input_path) > max_file_size:
+    if os.path.exists(input_url):
+        if os.path.getsize(input_url) > max_file_size:
             raise ValueError(f"File too large: max {max_file_size / MEGABYTE}MB")
     else:
-        raise FileNotFoundError(f"File not found: {input_path}")
+        raise FileNotFoundError(f"File not found: {input_url}")
 
-    validate_extension(input_path, supported_extensions)
+    validate_extension(input_url, supported_extensions)
 
     try:
-        with open(input_path, "rb") as img_file:
+        with open(input_url, "rb") as img_file:
             img_bytes = img_file.read()
             base64_data = base64.b64encode(img_bytes).decode("utf-8")
 
@@ -31,16 +31,16 @@ def _process_input(
         raise ValueError(f"Error occurred while processing the file: {e}")
 
 
-def validate_extension(input_path: str, supported_extensions: List[str]) -> None:
-    file_ext = input_path.lower().split(".")[-1]
+def validate_extension(input_url: str, supported_extensions: List[str]) -> None:
+    file_ext = input_url.lower().split(".")[-1]
     if file_ext not in supported_extensions:
         supported = ", ".join([f".{ext}" for ext in supported_extensions])
         raise ValueError(f"Unsupported image extension. supported: {supported}")
 
 
 def create_message(
-    input_path: str, supported_extensions: List[str], max_file_size: int
+    input_url: str, supported_extensions: List[str], max_file_size: int
 ) -> dict:
-    url = _process_input(input_path, supported_extensions, max_file_size)
+    url = _process_input(input_url, supported_extensions, max_file_size)
 
     return {"type": "image_url", "image_url": {"url": url}}
