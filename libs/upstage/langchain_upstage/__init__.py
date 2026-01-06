01a57932cc00a9400d80f6e081ee2b1cc05d921f
김주호 <eora21@naver.com>
2026-01-06 14:33:43 +0900

    feat: Export Information Extraction Tools
    - Export UpstagePrebuiltInformationExtraction
    - Export UpstageUniversalInformationExtraction

--- libs/upstage/langchain_upstage/__init__.py
+++ libs/upstage/langchain_upstage/__init__.py
@@ -2,12 +2,8 @@ from langchain_upstage.chat_models import ChatUpstage
 from langchain_upstage.document_parse import UpstageDocumentParseLoader
 from langchain_upstage.document_parse_parsers import UpstageDocumentParseParser
 from langchain_upstage.embeddings import UpstageEmbeddings
-from langchain_upstage.prebuilt_information_extraction import (
-    UpstagePrebuiltInformationExtraction,
-)
-from langchain_upstage.universal_information_extraction import (
-    UpstageUniversalInformationExtraction,
-)
+from langchain_upstage.tools.prebuilt_information_extraction import UpstagePrebuiltInformationExtraction
+from langchain_upstage.tools.universal_information_extraction import UpstageUniversalInformationExtraction
 
 __all__ = [
     "ChatUpstage",
